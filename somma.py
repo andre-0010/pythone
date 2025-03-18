3f784f95c3849d56e04503c99b8d64fc5ff96dca
Temp mail <fekew42126@doishy.com>
2025-03-18 13:44:32 +0100

    somma in una print

--- somma.py
+++ somma.py
@@ -1,5 +1,9 @@
-valore_1 , valore_2 = input("passami il primo valore: "),input("passami il secondo valore: ")
-v1=int(valore_1)
-v2=int(valore_2)
-print("la somma è", v1+v2)
-print("la somma è", int(valore_1) + int(valore_2))
+print(f"La somma è {int(input('Passami il primo valore: ')) + int(input('Passami il secondo valore: '))}")
+
+#valore_1 , valore_2 = input("passami il primo valore: "),input("passami il secondo valore: ")
+#v1=int(valore_1)
+#v2=int(valore_2)
+#print("la somma è", v1+v2)
+#print("la somma è", int(valore_1) + int(valore_2))
+
+
