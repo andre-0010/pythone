4ba38c066b798a7f9aa0ec913ee73b941724316c
Temp mail <fekew42126@doishy.com>
2025-03-18 13:05:51 +0100

    somma

--- somma.py
+++ somma.py
@@ -1,4 +1,5 @@
 valore_1 , valore_2 = input("passami il primo valore: "),input("passami il secondo valore: ")
-v1= int(valore_1)
+v1=int(valore_1)
 v2=int(valore_2)
 print("la somma è", v1+v2)
+print("la somma è", int(valore_1) + int(valore_2))
