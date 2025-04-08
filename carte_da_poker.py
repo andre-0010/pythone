251488925fd76f2dca690b53673c5fda1857f2cd
Temp mail <fekew42126@doishy.com>
2025-04-08 11:09:19 +0200

    massimo.py

---
+++ carte_da_poker.py
@@ -0,0 +1,4 @@
+#identificare i caratteri di unicode che rappresentato i quattro semi del poker, solo bordo
+carte="♢ ♧ ♤ ♡"
+print(f"i semi delle carte da poker sono {carte}".encode())
+print(carte)
--- giorni.py
+++ giorni.py
@@ -2,3 +2,7 @@ giorni="lunedì   martedì  mercoledìgiovedi  venerdì  sabato   domenica "
 index=int(input("giorno"))
 p=(index-1)*9
 print(giorni[p:p+9])
+
+
+formato="{:<-9s}"
+print(formato % giorni[p])
