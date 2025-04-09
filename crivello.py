600aae5a31134bb245a581fa19516a88b129b8a6
Bob <andreakanakciu@gmail.com>
2025-04-09 13:15:30 +0200

    Add files via upload

--- crivello.py
+++ crivello.py
@@ -2,13 +2,12 @@ n=100
 lista={x for x in range(2,n+1)}
 
 for x in range(2,n+1):
-    print(x)
     for y in range(2,int(x**0.5)+1):
         if x%y == 0:
             try:
                 lista.discard(x)
             except:
-                print("errore", end="")
+                print("errore","y:",y,"x:",x)
 
 print(lista, len(lista))
 
@@ -18,9 +17,10 @@ print(lista, len(lista))
 
 # for x in lista:
 #     for y in range(2,int(x**0.5)+1):
-#         if x%y == 0:
+#         if y%x != 0:
 #             try:
-#                 lista.remove(x)
+#                 lista.remove(y)
+#                 break
 #             except:
 #                 print("y:",y,"x:",x)
 
