942967d2491989fdc82c906e4c3cdbc6a1b1875a
Bob <andreakanakciu@gmail.com>
2025-04-30 10:23:38 +0200

    Add files via upload

---
+++ estrai.py
@@ -0,0 +1,21 @@
+import urllib.request
+url = "https://www.comuni-italiani.it/province.html"
+response = urllib.request.urlopen(url)
+theBytes = response.read()
+text = theBytes.decode(encoding="iso-8859-1")
+import bs4
+doc = bs4.BeautifulSoup(text, features="html.parser")
+elems = doc.find_all("table")
+table = elems[3]
+for tr in table.contents[2:-2]:
+    if type(tr) == bs4.element.Tag :
+        tds = tr.contents
+        kmq = int(tds[4].get_text().replace(".",""))
+        prov = tds[1].get_text()
+        resi = int(tds[2].get_text().replace(".",""))
+        sigl = tds[7].get_text()
+        densita = round(float((tds[5].get_text().replace(".","")).replace(",",".")), 1)
+        densitaCalcolata = round(resi/kmq, 1)
+        print(f"{sigl} {prov:25s} {resi:9d}  {kmq:6d} {densitaCalcolata}")
+        if densitaCalcolata != densita :
+            print(f"Densita calcolata non corrisponde {densitaCalcolata} {densita}")
